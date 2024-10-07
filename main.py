0320b657d903790e49e254d77c4ed8e9f4d8ee4b
docv77220 <docvickers7@gmail.com>
2024-10-07 09:13:07 -0700

    system-test

---
+++ main.py
@@ -0,0 +1,10 @@
+# Name: Drake Vickers
+# OSU Email: vickerdr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 10/7
+# Description:
+
+
+def gradescope_test():
+    return 42
